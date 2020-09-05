35efd48042a41b80af0202e7e01ba2656c378bc6
Alla Kuzminova <alchik@gmail.com>
2020-09-05 18:10:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,11 +10,8 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-requires "pry"
 def speak_to_grandma(phrase)
   if(phrase == "I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN!"
   end
-  binding.pry
 end
